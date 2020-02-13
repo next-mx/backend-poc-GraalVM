827ca5cd3c3d82558d0c251e7e4a1e65730ae8b3
Crisanto Jeronimo Garcia <crisanto.jeronimo.next@bbva.com>
2020-02-12 21:42:18 -0600

    Implementa creación y consulta de backup

--- js-node-service/app.js
+++ js-node-service/app.js
@@ -1,7 +1,10 @@
 const Express = require("express");
 const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
-const ObjectId = require("mongodb").ObjectID;
+var schedule = require('node-schedule');
+const csv_writer = require('csv-writer').createObjectCsvWriter;
+const csv_reader = require('csv-parser');
+const fs = require('fs');
 var ObjectID = require('mongodb').ObjectID;
 var url = "mongodb://poc-user:6r44lvM#@127.0.0.1:27017/poc-db";
 
@@ -14,7 +17,7 @@ COMMENTS_COLLECTION = "Comments";
 const SERVER_PORT = 8080;
 
 var app = Express();
-var database,collection;
+var database;
 
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
@@ -26,6 +29,33 @@ app.listen(SERVER_PORT, () => {
 	  log.info("Server running and listening on port: " + SERVER_PORT);
  	});
 });
+
+
+
+
+/* =========================== BACKUP ============================*/
+app.post('/pocgraalvm/api/v1/movies/backup', (request, response) => {
+	log.info("Agendando respaldo de películas");
+	scheduleBackup();
+
+	response.status(201).send({"message": 'Respaldo agendado exitosamente', "result": null})
+});
+
+
+app.get('/pocgraalvm/api/v1/movies/backup/', (request, response) => {
+	log.info("Consultando respaldo de películas");
+	csvData = []
+
+	fs.createReadStream('movies_catalog.csv')
+	.pipe(csv_reader())
+	.on('data', (row) => {
+		csvData.push(JSON.parse(row.MOVIE));
+	})
+	.on('end', () => {
+		response.status(200).send({"message": 'Respaldo consultado exitosamente', "result": csvData})
+	});
+});
+
 
 
 
@@ -45,7 +75,7 @@ app.post('/pocgraalvm/api/v1/movies/', function(request, response) {
 app.get('/pocgraalvm/api/v1/movies/', (request, response) => {
 	log.info("Consultando listado de películas");
 	let dbquery = {};
-	database.collection("Movies").find(dbquery).toArray(function(error, movies) {
+	database.collection(MOVIES_COLLECTION).find(dbquery).toArray(function(error, movies) {
 		if(error) {
 			log.error("Error de BD", error)
 			return response.status(500).send({"message": 'Error ocurrido al procesar su petición', "result": null})	
@@ -55,29 +85,29 @@ app.get('/pocgraalvm/api/v1/movies/', (request, response) => {
 });
 
 
-app.get('/pocgraalvm/api/v1/movies/:movieId', (request, response) => {
-	var movieId = request.params.movieId;
+app.get('/pocgraalvm/api/v1/movies/:movieId/', (request, response) => {
+	let movieId = request.params.movieId;
 	log.info("Consultando película ID " + movieId);
 	
-	var o_id = new ObjectID(movieId);
-	var dbquery = { _id:{$eq:o_id} };
+	let o_id = new ObjectID(movieId);
+	let dbquery = { _id:{$eq:o_id} };
 
 	database.collection(MOVIES_COLLECTION).findOne(dbquery, (error, movie) => {
 		if(error) {
 			log.error("Error de BD", error)
 			return response.status(500).send({"message": 'Error ocurrido al procesar su petición', "result": null})	
 		}
-		response.status(200).send({"message": 'Película agregada exitosamente', "result": movie})
+		response.status(200).send({"message": 'Película consultada exitosamente', "result": movie})
 	});
 });
 
 
 app.put('/pocgraalvm/api/v1/movies/:movieId', (request, response) => {
-	var movieId = request.params.movieId;
+	let movieId = request.params.movieId;
 	log.info("Modificando película ID " + movieId);
-	var o_id = new ObjectID(movieId);
- 	var dbquery = { _id:{$eq:o_id} };
- 	var newvalues = {$set: request.body};
+	let o_id = new ObjectID(movieId);
+ 	let dbquery = { _id:{$eq:o_id} };
+ 	let newvalues = {$set: request.body};
  	database.collection(MOVIES_COLLECTION).replaceOne(dbquery, newvalues,  (error, obj) => {
 		if(error) {
 			log.error("Error de BD", error)
@@ -89,11 +119,11 @@ app.put('/pocgraalvm/api/v1/movies/:movieId', (request, response) => {
 
 
 app.patch('/pocgraalvm/api/v1/movies/:movieId', (request, response) => {
-	var movieId = request.params.movieId;
+	let movieId = request.params.movieId;
 	log.info("Actualizando película ID " + movieId);
-	var o_id = new ObjectID(movieId);
- 	var dbquery = { _id:{$eq:o_id} };
- 	var newvalues = {$set: request.body};
+	let o_id = new ObjectID(movieId);
+ 	let dbquery = { _id:{$eq:o_id} };
+ 	let newvalues = {$set: request.body};
  	database.collection(MOVIES_COLLECTION).updateOne(dbquery, newvalues,  (error, obj) => {
 		if(error) {
 			log.error("Error de BD", error)
@@ -105,23 +135,27 @@ app.patch('/pocgraalvm/api/v1/movies/:movieId', (request, response) => {
 
 
 app.delete('/pocgraalvm/api/v1/movies/:movieId', function(request, response){
-	var movieId = request.params.movieId;
+	let movieId = request.params.movieId;
 	log.info("Eliminando película ID " + movieId);
- 	var o_id = new ObjectID(movieId);
-  	var dbquery = { _id:{$eq:o_id} };
+ 	let o_id = new ObjectID(movieId);
+  	let dbquery = { _id:{$eq:o_id} };
   	database.collection(MOVIES_COLLECTION).deleteOne(dbquery, (error, obj) => {
 		if (error) {
 			log.error("Error de BD", error)
 			return response.status(500).send({"message": 'Error ocurrido al procesar su petición', "result": null})	
 		}
 		if(obj.result.n == 1){
-			response.status(200).send({"message": 'Película consultada exitosamente', "result": null})
+			response.status(200).send({"message": 'Película eliminada exitosamente', "result": null})
 		}else{
 			responseDTO.message = 'Película no encontrada';
 			response.status(404).send({"message": 'Película no encontrada', "result": null})
 		}
   	});
 });
+
+
+
+
 
 
 
@@ -144,9 +178,9 @@ app.post('/pocgraalvm/api/v1/movies/:movieId/comments', (request, response) => {
 
 
 app.get('/pocgraalvm/api/v1/movies/:movieId/comments/', (request, response) => {
-	var movieId = request.params.movieId;
+	let movieId = request.params.movieId;
 	log.info("Consultando comentarios de la película ID " + movieId);
-	var dbquery = { movie_id:{$eq:movieId}};
+	let dbquery = { movie_id:{$eq:movieId}};
 
 	database.collection(COMMENTS_COLLECTION).find(dbquery).toArray((error, comments) => {
 		if(error) {
@@ -160,10 +194,10 @@ app.get('/pocgraalvm/api/v1/movies/:movieId/comments/', (request, response) => {
 
 
 app.get('/pocgraalvm/api/v1/movies/:movieId/comments/:commentId', (request, response) => {
-	var commentId = request.params.commentId;
+	let commentId = request.params.commentId;
 	log.info("Consultando comentario ID " + commentId);
-	var o_id = new ObjectID(commentId);
-	var dbquery = { _id:{$eq:o_id} };
+	let o_id = new ObjectID(commentId);
+	let dbquery = { _id:{$eq:o_id} };
 
 	database.collection(COMMENTS_COLLECTION).findOne(dbquery, (error, comment) => {
 		if(error) {
@@ -176,11 +210,11 @@ app.get('/pocgraalvm/api/v1/movies/:movieId/comments/:commentId', (request, resp
 
 
 app.put('/pocgraalvm/api/v1/movies/:movieId/comments/:commentId', (request, response) => {
-	var commentId = request.params.commentId;
+	let commentId = request.params.commentId;
 	log.info("Modificando comentario ID " + commentId + " de la película ID " + request.params.movieId); 
-	var o_id = new ObjectID(commentId);
- 	var dbquery = { _id:{$eq:o_id} };
- 	var newvalues = {$set: request.body};
+	let o_id = new ObjectID(commentId);
+ 	let dbquery = { _id:{$eq:o_id} };
+ 	let newvalues = {$set: request.body};
  	database.collection(COMMENTS_COLLECTION).replaceOne(dbquery, newvalues,  (error, obj) => {
 		if(error) {
 			log.error("Error de BD", error)
@@ -192,11 +226,11 @@ app.put('/pocgraalvm/api/v1/movies/:movieId/comments/:commentId', (request, resp
 
 
 app.patch('/pocgraalvm/api/v1/movies/:movieId/comments/:commentId', (request, response) => {
-	var commentId = request.params.commentId;
+	let commentId = request.params.commentId;
 	log.info("Actualizando comentario ID " + commentId + " de la película ID " + request.params.movieId); 
-	var o_id = new ObjectID(commentId);
- 	var dbquery = { _id:{$eq:o_id} };
- 	var newvalues = {$set: request.body};
+	let o_id = new ObjectID(commentId);
+ 	let dbquery = { _id:{$eq:o_id} };
+ 	let newvalues = {$set: request.body};
  	database.collection(COMMENTS_COLLECTION).updateOne(dbquery, newvalues,  (error, obj) => {
 		if(error) {
 			log.error("Error de BD", error)
@@ -208,10 +242,10 @@ app.patch('/pocgraalvm/api/v1/movies/:movieId/comments/:commentId', (request, re
 
 
 app.delete('/pocgraalvm/api/v1/movies/:movieId/comments/:commentId', (request, response) => {
-	var commentId = request.params.commentId;
+	let commentId = request.params.commentId;
 	log.info("Eliminando comentario ID " + commentId + " de la película ID " + request.params.movieId); 
-	var o_id = new ObjectID(commentId);
- 	var dbquery = { _id:{$eq:o_id} };
+	let o_id = new ObjectID(commentId);
+ 	let dbquery = { _id:{$eq:o_id} };
   	database.collection(COMMENTS_COLLECTION).deleteOne(dbquery, function(error, obj) {
 		if (error) {
 			log.error("Error de BD", error)
@@ -225,4 +259,64 @@ app.delete('/pocgraalvm/api/v1/movies/:movieId/comments/:commentId', (request, r
 	});
 });
 
+
+
+
+
+
+
+function scheduleBackup() {
+	let startTime = new Date(Date.now() + 4000);
+	let endTime = new Date(startTime.getTime() + 1000);
+	schedule.scheduleJob({ start: startTime, end: endTime, rule: '*/1 * * * * *' }, async function(){
+		try {
+			writeBackupFile();
+		}
+		catch (err) {
+			log.console.warn(err);
+		}
+	});
+}
+
+function writeBackupFile() {
+	const csvWriter = csv_writer({
+		path: 'movies_catalog.csv',
+		header: [
+		  {id: 'Id', title: 'ID'},
+		  {id: 'Movie', title: 'MOVIE'}
+		]
+	});
+	let csvData = [];
+
+	let dbquery = {};
+	let numberOfMovies;
+	database.collection(MOVIES_COLLECTION).countDocuments(dbquery, function(error, numberOfDocumments){
+		if(error) return callback(error);
+		numberOfMovies = numberOfDocumments;
+	});
+	
+	database.collection(MOVIES_COLLECTION).find(dbquery).toArray(function(error, movies) {
+		if(error) {
+			log.error("Error de BD", error)
+			return response.status(500).send({"message": 'Error ocurrido al procesar su petición', "result": null})	
+		}
+
+		movies.forEach((movie, index) => {
+			let id = Date.now(); // Unix timestamp in milliseconds
+			id = id + index;
+
+			dbquery = { movie_id:{$eq:movie._id.toString()}};
+			database.collection(COMMENTS_COLLECTION).find(dbquery).toArray((error, comments) => {
+				if(error) return callback(error);
+				let numberOfComments = comments.length;
+				id = id * (numberOfComments>0 ? numberOfComments : 1) / numberOfMovies;
+				
+				csvData.push({'Id': id, 'Movie': JSON.stringify(movie)});
+				csvWriter
+				.writeRecords(csvData)
+				.then(()=> console.log('The CSV file was written successfully'));
+			});
+		});			
+	});
+}
 
